c08ef28c85ed2572a433a24c3e43a62a0a294981
vishalamler <64705742+vishalamler@users.noreply.github.com>
2020-05-03 00:41:09 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,11 @@
+// custom javascript 
+
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
